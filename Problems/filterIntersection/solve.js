7c0e2d3677822f6fee317e0ba0d7dbed3e6193e9
white9p <kmjkmj9905@naver.com>
2021-08-22 23:56:46 +0900

    solved filternintersection

--- Problems/filterIntersection/solve.js
+++ Problems/filterIntersection/solve.js
@@ -1,3 +1,18 @@
-function solution(inputArray1, inputArray2) {}
+function solution(inputArray1, inputArray2) {
+  const newArray = [];
+  for (let i = 0; i < inputArray1.length; i++) {
+    if (!newArray.includes(inputArray1[i])) {
+      newArray.push(inputArray1[i]);
+    }
+  }
+
+  const resultArray = []
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray.includes(inputArray2[i])) {
+      resultArray.push(inputArray2[i]);
+    }
+  }
+  return resultArray;
+}
 
 exports.solution = solution;
